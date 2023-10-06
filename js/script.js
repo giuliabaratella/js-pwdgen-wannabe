af1927e43b6c75398dcedc3bda81ad8fc9a6d778
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-06 15:28:04 +0200

    added js variables to generate password

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+const numeroFisso = 21;
+
+let nome = prompt ("Qual è il tuo nome?");
+
+let cognome = prompt ("Qual è il tuo cognome?");
+
+let colorePreferito = prompt ("Qual è il tuo colore preferito?");
+
+let password = nome + cognome + colorePreferito + numeroFisso;
+console.log (password);
+
+document.getElementById("pw-result").innerHTML = password;
